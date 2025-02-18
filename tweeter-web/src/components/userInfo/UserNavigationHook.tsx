7b3ee72006fe138dd7ffc8f8e16fa72f682d9e37
Tad Rosenberg <tad@swelle.io>
2025-02-18 09:41:04 -0700

    finished navigationHook and removed references

--- tweeter-web/src/components/userInfo/UserNavigationHook.tsx
+++ tweeter-web/src/components/userInfo/UserNavigationHook.tsx
@@ -1,4 +1,4 @@
-import { AuthToken, FakeData, User } from "tweeter-shared";
+import { AuthToken, User } from "tweeter-shared";
 import useInfoHook from "./UserInfoHook";
 import useToastListener from "../toaster/ToastListenerHook";
 import {
