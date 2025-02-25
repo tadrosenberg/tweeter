8c6cddcd82bf1d9ab06ebf39d6e9ac381379ba74
Tad Rosenberg <tad@swelle.io>
2025-02-24 18:11:02 -0700

    fixed imageHandling

--- tweeter-web/src/components/authentication/register/Register.tsx
+++ tweeter-web/src/components/authentication/register/Register.tsx
@@ -3,12 +3,13 @@ import "bootstrap/dist/css/bootstrap.css";
 import { ChangeEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import AuthenticationFormLayout from "../AuthenticationFormLayout";
-import { Buffer } from "buffer";
 import useToastListener from "../../toaster/ToastListenerHook";
 import AuthenticationFields from "../AuthenticationFields";
 import useInfoHook from "../../userInfo/UserInfoHook";
-import { RegisterPresenter } from "../../../presenter/RegisterPresenter";
-import { AuthView } from "../../../presenter/Presenter";
+import {
+  RegisterPresenter,
+  RegisterView,
+} from "../../../presenter/RegisterPresenter";
 
 const Register = () => {
   const [firstName, setFirstName] = useState("");
@@ -46,48 +47,23 @@ const Register = () => {
     handleImageFile(file);
   };
 
-  //pull this out into presenter
   const handleImageFile = (file: File | undefined) => {
-    if (file) {
-      setImageUrl(URL.createObjectURL(file));
-
-      const reader = new FileReader();
-      reader.onload = (event: ProgressEvent<FileReader>) => {
-        const imageStringBase64 = event.target?.result as string;
-
-        // Remove unnecessary file metadata from the start of the string.
-        const imageStringBase64BufferContents =
-          imageStringBase64.split("base64,")[1];
-
-        const bytes: Uint8Array = Buffer.from(
-          imageStringBase64BufferContents,
-          "base64"
-        );
-
-        setImageBytes(bytes);
-      };
-      reader.readAsDataURL(file);
-
-      // Set image file extension (and move to a separate method)
-      const fileExtension = getFileExtension(file);
-      if (fileExtension) {
-        setImageFileExtension(fileExtension);
-      }
-    } else {
-      setImageUrl("");
-      setImageBytes(new Uint8Array());
-    }
+    presenter.handleImageFile(file);
   };
 
   const getFileExtension = (file: File): string | undefined => {
     return file.name.split(".").pop();
   };
 
-  const listener: AuthView = {
+  const listener: RegisterView = {
     setIsLoading: setIsLoading,
     updateUserInfo: updateUserInfo,
     displayErrorMessage: displayErrorMessage,
     navigate: navigate,
+    setImageUrl: setImageUrl,
+    setImageBytes: setImageBytes,
+    setImageFileExtension: setImageFileExtension,
+    getFileExtension: getFileExtension,
   };
 
   const [presenter] = useState(new RegisterPresenter(listener));
--- tweeter-web/src/presenter/RegisterPresenter.ts
+++ tweeter-web/src/presenter/RegisterPresenter.ts
@@ -1,11 +1,19 @@
+import { Buffer } from "buffer";
 import { UserService } from "../model/service/UserService";
 import { AuthPresenter } from "./AuthPresenter";
 import { AuthView } from "./Presenter";
 
-export class RegisterPresenter extends AuthPresenter<AuthView> {
+export interface RegisterView extends AuthView {
+  setImageUrl: (url: string) => void;
+  setImageBytes: (bytes: Uint8Array) => void;
+  setImageFileExtension: (extension: string) => void;
+  getFileExtension: (file: File) => string | undefined;
+}
+
+export class RegisterPresenter extends AuthPresenter<RegisterView> {
   private userService: UserService;
 
-  public constructor(view: AuthView) {
+  public constructor(view: RegisterView) {
     super(view);
     this.userService = new UserService();
   }
@@ -50,5 +58,35 @@ export class RegisterPresenter extends AuthPresenter<AuthView> {
       rememberMe,
       "register user"
     );
+  }
+
+  public handleImageFile(file: File | undefined) {
+    if (file) {
+      this.view.setImageUrl(URL.createObjectURL(file));
+
+      const reader = new FileReader();
+      reader.onload = (event: ProgressEvent<FileReader>) => {
+        const imageStringBase64 = event.target?.result as string;
+
+        const imageStringBase64BufferContents =
+          imageStringBase64.split("base64,")[1];
+
+        const bytes: Uint8Array = Buffer.from(
+          imageStringBase64BufferContents,
+          "base64"
+        );
+
+        this.view.setImageBytes(bytes);
+      };
+      reader.readAsDataURL(file);
+
+      const fileExtension = this.view.getFileExtension(file);
+      if (fileExtension) {
+        this.view.setImageFileExtension(fileExtension);
+      }
+    } else {
+      this.view.setImageUrl("");
+      this.view.setImageBytes(new Uint8Array());
+    }
   }
 }
