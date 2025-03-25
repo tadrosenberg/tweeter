0230c8cd9b92bfb4fd784122a9770f312f7b7c76
Tad Rosenberg <tad@swelle.io>
2025-03-24 19:06:05 -0600

    built first lambda and tested successfully

--- tweeter-shared/src/index.ts
+++ tweeter-shared/src/index.ts
@@ -1,9 +1,18 @@
+//Domain Classes
 export { Follow } from "./model/domain/Follow";
 export { PostSegment, Type } from "./model/domain/PostSegment";
 export { Status } from "./model/domain/Status";
 export { User } from "./model/domain/User";
 export { AuthToken } from "./model/domain/AuthToken";
 
-// All classes that should be avaialble to other modules need to exported here. export * does not work when 
-// uploading to lambda. Instead we have to list each export.
+//Other
 export { FakeData } from "./util/FakeData";
+
+//Requests
+export type { PagedUserItemRequest } from "./model/net/request/PagedUserItemRequest";
+
+//Responses
+export type { PagedUserItemResponse } from "./model/net/response/PagedUserItemResponse";
+
+//DTOs
+export type { UserDto } from "./model/dto/UserDto";
---
+++ tweeter-shared/src/model/dto/UserDto.ts
@@ -0,0 +1,6 @@
+export interface UserDto {
+  readonly alias: string;
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly imageUrl: string;
+}
