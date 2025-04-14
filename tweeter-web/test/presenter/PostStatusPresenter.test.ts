1d3e4e2c68e4a6be6cdab6bc5ad8e772d5fb38f1
Tad Rosenberg <tad@swelle.io>
2025-04-14 16:27:34 -0600

    added tests

--- tweeter-web/test/presenter/PostStatusPresenter.test.ts
+++ tweeter-web/test/presenter/PostStatusPresenter.test.ts
@@ -1,94 +1,58 @@
+import { LoginRequest, PagedStatusItemRequest, User } from "tweeter-shared";
 import {
   PostStatusPresenter,
   PostStatusView,
 } from "../../src/presenter/PostStatusPresenter";
-import { StatusService } from "../../src/model/service/StatusService";
-import {
-  anything,
-  capture,
-  instance,
-  mock,
-  spy,
-  verify,
-  when,
-} from "@typestrong/ts-mockito";
-import { AuthToken, User } from "tweeter-shared";
+import { instance, mock, verify, spy, when } from "@typestrong/ts-mockito";
+import { ServerFacade } from "../../src/network/ServerFacade";
+
+console.log = jest.fn();
 
 describe("PostStatusPresenter", () => {
   let mockPostStatusView: PostStatusView;
   let postStatusPresenter: PostStatusPresenter;
-  let mockStatusService: StatusService;
-  const testUser = new User("firstName", "lastName", "alias", "imageUrl");
-  const postMessage = "Hello, world!";
-  const testAuthToken = new AuthToken("token", Date.now());
+  let serverFacade: ServerFacade;
+  let loggedInUser: User;
+  let authToken: any;
+  const postString = "hello world";
+  const mockUser = new User(
+    "Tom",
+    "Brady",
+    "@therealking",
+    "https://tads-tweeter.s3.us-east-1.amazonaws.com/images/@therealking.jpeg"
+  );
+  const loginRequest: LoginRequest = {
+    alias: "@therealking",
+    password: "therealking",
+  };
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    serverFacade = new ServerFacade();
+    [loggedInUser, authToken] = await serverFacade.login(loginRequest);
+
     mockPostStatusView = mock<PostStatusView>();
-    const postStatusViewInstance = instance(mockPostStatusView);
+    when(mockPostStatusView.getAuthToken()).thenReturn(authToken);
+    when(mockPostStatusView.getCurrentUser()).thenReturn(loggedInUser);
+    const mockPostStatusViewInstance = instance(mockPostStatusView);
 
-    const postStatusPresenterSpy = spy(
-      new PostStatusPresenter(postStatusViewInstance)
+    const PostStatusPresenterSpy = spy(
+      new PostStatusPresenter(mockPostStatusViewInstance)
     );
-    postStatusPresenter = instance(postStatusPresenterSpy);
-
-    mockStatusService = mock<StatusService>();
-    const mockStatusServiceInstance = instance(mockStatusService);
-
-    when(postStatusPresenterSpy.statusService).thenReturn(
-      mockStatusServiceInstance
-    );
-    when(mockPostStatusView.getAuthToken()).thenReturn(testAuthToken);
+    postStatusPresenter = instance(PostStatusPresenterSpy);
   });
 
-  it("presenter tells the view to display a posting message", async () => {
-    await postStatusPresenter.submitPost("Hello, world!");
-    verify(
-      mockPostStatusView.displayInfoMessage("Posting status...", 0)
-    ).once();
-  });
-
-  it("presenter calls postStatus on the post status service with the correct status string and auth token", async () => {
-    await postStatusPresenter.submitPost("Hello, world!");
-
-    let [capturedAuthToken, capturedStatus] = capture(
-      mockStatusService.postStatus
-    ).last();
-    console.log(capturedAuthToken);
-    console.log(capturedStatus);
-
-    expect(capturedAuthToken).toEqual(testAuthToken);
-    expect(capturedStatus.post).toEqual(postMessage);
-
-    verify(mockStatusService.postStatus(anything(), anything())).once();
-  });
-
-  it("presenter tells the view to clear the last info message, clear the post, and display a status posted message", async () => {
-    await postStatusPresenter.submitPost("Hello, world!");
-
-    verify(mockPostStatusView.clearLastInfoMessage()).once();
-    verify(mockPostStatusView.setPost("")).once();
+  it("submit post, check for success message and successful story post", async () => {
+    await postStatusPresenter.submitPost(postString);
+    const storyRequest: PagedStatusItemRequest = {
+      token: authToken.token,
+      userAlias: "@therealking",
+      pageSize: 10,
+      lastItem: null,
+    };
+    const [statuses, hasMore] = await serverFacade.getMoreStories(storyRequest);
+    expect(statuses[0].post).toEqual(postString);
     verify(
       mockPostStatusView.displayInfoMessage("Status posted!", 2000)
     ).once();
-    verify(mockPostStatusView.displayErrorMessage(anything())).never();
-  });
-
-  it("presenter displays an error message and clears last info message, doesn't clear post or display status posted message", async () => {
-    const error = new Error("An error occured");
-    when(mockStatusService.postStatus(anything(), anything())).thenThrow(error);
-
-    await postStatusPresenter.submitPost("Hello, world!");
-
-    verify(
-      mockPostStatusView.displayErrorMessage(
-        "Failed to post the status because of exception: An error occured"
-      )
-    );
-
-    verify(mockPostStatusView.clearLastInfoMessage()).once();
-    verify(
-      mockPostStatusView.displayInfoMessage("Status posted!", 2000)
-    ).never();
-    verify(mockPostStatusView.setPost("")).never();
   });
 });
